bd3035f34da184461069e9ced321e1e39c2dd465
vaticorp <vaticorp@mail.ru>
2018-01-20 14:26:55 +0400

    1.1. Создание Hello World[#141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,22 @@
+
+package ru.job4j;
+
+/**
+*This is my first class on course job4j, but not in my programming practice
+*time:   jan 20 2018
+*author: Sabirov Svytoslav
+*/
+
+public class Calculate {
+	
+	public String echo(String value){
+		return String.format("%s %s %s", value, value, value);		
+	}
+	
+	public static void main(String[] args){
+
+	    System.out.println("Hello world!");
+	
+	}
+	
+} 
