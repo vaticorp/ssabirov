2263c9ff9caae1cb001070eea59f36b46b2fa064
vaticorp <vaticorp@mail.ru>
2018-01-21 12:07:07 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,17 +2,21 @@
 package ru.job4j;
 
 /**
-*This is my first class on course job4j, but not in my programming practice(chapter_001)
-*time:   jan 20 2018
-*author: Sabirov Svytoslav
-*/
-
+ *This is my first class on course job4j, but not in my programming practice(chapter_001)
+ *@author Sabirov Svytoslav
+ *@since   20.01.2018
+ *@version 1
+ */
 public class Calculate {
 	
 	public String echo(String value){
 		return String.format("%s %s %s", value, value, value);		
 	}
 	
+	/**
+	 * Metod created for outputing string value on the screen
+	 *@ param - String[](default parameter) 
+	 */
 	public static void main(String[] args){
 
 	    System.out.println("Hello world!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,10 @@
+﻿
+/**
+* Package for calculate task.
+*
+* @author Svyatoslav Sabirov (vaticorp@mail.ru)
+* @version $Id$
+* @since 20.01.2017
+*/
+
+package ru.job4j
