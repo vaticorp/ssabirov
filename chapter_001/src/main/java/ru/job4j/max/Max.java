3f2906c77253bbd3b00de4285187c5b0a7e8f4b5
vaticorp <vaticorp@mail.ru>
2018-01-25 21:28:03 +0400

    3.4. Максимум из трех чисел [#190] (Апдейт исправления)

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -26,6 +26,7 @@ public class Max {
      * @return maximum value.
      */
     public int max(int first, int second, int third) {
-       return max(max(first, second), max(second, third));
+       return max(max(first, second), third);
     }
+
 }
