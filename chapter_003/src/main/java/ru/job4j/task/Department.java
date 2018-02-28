0a70ff00678d91aa462da0c79f514cf9e55a0231
vaticorp <vaticorp@mail.ru>
2018-03-01 00:25:40 +0400

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/main/java/ru/job4j/task/Department.java
@@ -0,0 +1,62 @@
+package ru.job4j.task;
+
+import java.util.*;
+
+/**
+ * This class represents sort of Department.
+ * @author Svyatoslav Sabirov.
+ * @since 28.02.2018
+ * @version 7.
+ */
+public class Department {
+
+    /**
+     * Сортирует массив подразделений.
+     * @param departments - массив подразделений.
+     * @param desc - по возврастанию/убыванию.
+     * @return - подразделения в нужном порядке.
+     */
+    public List<String> sort(String[] departments, boolean desc) {
+        List<String> departmentList = new ArrayList(Arrays.asList(departments));
+        ListIterator<String> departmentIterator = departmentList.listIterator();
+        while (departmentIterator.hasNext()) {
+            String currentDepartment = departmentIterator.next();
+            if (currentDepartment.indexOf("\\") > 0) {
+                String[] currentDepartments = currentDepartment.split("\\\\");
+                String value = "";
+                for (String part: currentDepartments) {
+                    value += value == "" ? part : "\\" + part;
+                    if (!departmentList.contains(value)) {
+                        departmentIterator.add(value);
+                    }
+                }
+            }
+        }
+        departmentList.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (desc) {
+                    return o1.compareTo(o2);
+                } else {
+                    char[] firstArray = o1.toCharArray();
+                    char[] secondArray = o2.toCharArray();
+                    int index = 0;
+                    int result = 0;
+                    while ((index != firstArray.length) && (index != secondArray.length)) {
+                        result = String.valueOf(secondArray[index]).compareTo(String.valueOf(firstArray[index]));
+                        if (result != 0) {
+                            break;
+                        }
+                        index++;
+                    }
+                    if (result == 0) {
+                        if (index != firstArray.length) { result = 1; }
+                        if (index != secondArray.length) { result = -1; }
+                    }
+                    return result;
+                }
+            }
+        });
+        return departmentList;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/task/DepartmentTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.task;
+
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This is test class for DepartmentTest.
+ * @author Svatoslav Sabirov.
+ * @version 11.
+ * @since 28.02.2018.
+ */
+public class DepartmentTest {
+    @Test
+    public void whenWeHaveOnlyArrayAndSortAsc() {
+        Department sortDepartment = new Department();
+        String[] department = new String[7];
+        department[0] = "K1\\SK1";
+        department[1] = "K1\\SK2";
+        department[2] = "K1\\SK1\\SSK1";
+        department[3] = "K1\\SK1\\SSK2";
+        department[4] = "K2";
+        department[5] = "K2\\SK1\\SSK1";
+        department[6] = "K2\\SK1\\SSK2";
+        assertThat("K1", is(sortDepartment.sort(department, true).get(0)));
+    }
+    @Test
+    public void whenWeHaveOnlyArrayAndSortDesc() {
+        Department sortDepartment = new Department();
+        String[] department = new String[7];
+        department[0] = "K1\\SK1";
+        department[1] = "K1\\SK2";
+        department[2] = "K1\\SK1\\SSK1";
+        department[3] = "K1\\SK1\\SSK2";
+        department[4] = "K2";
+        department[5] = "K2\\SK1\\SSK1";
+        department[6] = "K2\\SK1\\SSK2";
+        assertThat("K2", is(sortDepartment.sort(department, false).get(0)));
+    }
+}
