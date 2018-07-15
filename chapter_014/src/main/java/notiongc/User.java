1e63c78de354de53420195f70de99744f541d1a4
vaticorp <vaticorp@mail.ru>
2018-07-15 12:31:56 +0400

    Демонстрация работы GC [#1589]

---
+++ chapter_014/src/main/java/notiongc/User.java
@@ -0,0 +1,62 @@
+package notiongc;
+
+import java.util.ArrayList;
+
+/**
+ * This class represents user-model.
+ * @author Svyatoslav Sabirov.
+ * @since 15.07.2018
+ * @version 7.
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    /**
+     * Метод выводит текущую информацию о состоянии памяти.
+     */
+    public static void info() {
+        int mb = 1049600; //1049600
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("Used memory: " + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+        System.out.println("Free memory: " + runtime.freeMemory() / mb);
+        System.out.println("Total memory: " + runtime.totalMemory() / mb);
+        System.out.println("Max memory: " + runtime.maxMemory() / mb);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(String.format("Warning! Destroying %s...", this));
+        super.finalize();
+    }
+    public static void main(String[] args) {
+        System.out.println("Start program!");
+        ArrayList<User> users = new ArrayList<User>();
+        info();
+        for (int index = 0; index < 29239; index++) {
+            //User currentUser = new User(String.format("Petr %s", index), 28);
+            //System.out.println(String.format("Create new object %s", currentUser));
+            users.add(new User(String.format("Petr %s", index), 28));
+            System.out.println(String.format("Добавлен объект номер %s", index));
+            //currentUser = null;
+            //System.gc();
+        }
+        info();
+        //System.gc();
+        System.out.println("Finish program!");
+    }
+}
---
+++ chapter_014/src/main/java/notiongc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package represents notion of garbage-collection.
+ * @author Svyatoslav Sabirov.
+ * @since 15.07.2018
+ * @version 5.
+ */
+
+package notiongc;
