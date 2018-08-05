70d6f1a3a07906a35882ef2bae04b895f0531456
vaticorp <vaticorp@mail.ru>
2018-08-05 10:38:51 +0400

    2. Удаление запрещенных слов [#859]

--- chapter_015/src/main/java/io/Check.java
+++ chapter_015/src/main/java/io/Check.java
@@ -1,6 +1,8 @@
 package io;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * This class represents class for input-check;
@@ -9,6 +11,7 @@ import java.io.*;
  * @version 9.
  */
 public class Check {
+
     public boolean isNumber(InputStream in) {
         int value = 0;
         try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));) {
@@ -19,8 +22,21 @@ public class Check {
         return (value % 2) == 0;
     }
 
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));) {
+            String newString = bufferedReader.readLine();
+            for (String value : abuse) {
+                newString.replaceAll(value, "");
+            }
+            out.write(newString.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         Check check = new Check();
-        System.out.println(check.isNumber(System.in));
+        //System.out.println(check.isNumber(System.in));
+        //check.dropAbuses(System.in, System.o);
     }
 }
